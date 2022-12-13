11bd636ff951fe22feb05be9526e20f11afbdea9
Karim_Gamal0 <karim.gamal.cs@gmail.com>
2022-12-13 15:44:28 +0200

    add producers and cinemas data

--- eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -15,6 +15,7 @@ namespace eTickets.Data.Base
         {
             _context = context;
         }
+
         public async Task AddAsync(T entity)
         {
             await _context.Set<T>().AddAsync(entity);
